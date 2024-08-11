9aa02f6de2e4d73ef320f8eedf2d505e75e6704f
Pruthviraj Gite <pruthviraj2400@gmail.com>
2024-08-11 19:23:55 +0000

    customer_order

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,4 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(materialized="table") }}
 
-select * from {{ source('jaffle_shop', 'customers') }}
+select id as customer_id, first_name, last_name
+from {{ source("jaffle_shop", "customers") }}
